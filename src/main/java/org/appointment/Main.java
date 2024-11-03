c65b57b4e9853ec1726fcd8a32c289a50f7b05cf
debagnika <debagnika@gmail.com>
2024-11-03 21:16:40 +0530

    setup complete

--- src/main/java/org/appointment/Main.java
+++ src/main/java/org/appointment/Main.java
@@ -1,8 +1,13 @@
 package org.appointment;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
     }
 }
